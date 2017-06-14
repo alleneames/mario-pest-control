8fea2a88db5a04fe2cee3d38e7784aaea9317f3b
Allen Eames <aeames@MacBook-Pro.local>
2017-06-14 13:10:36 -0600

    modified buttons

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
-var goombaCoin=5;
+var goombaCoin = 5;
+var bobOmbCoin = 7;
+var cheepCheepsCoin = 11;
 var goombaTotal= 0;
-//var input = Number(document.querySelector("#gInput").value);
-
-goomba function(){
+var bobOmbTotal = 0;
+var cheepCheepTotal = 0;
+//goomba function(){
     document.querySelector("#gButton").addEventListener("click", function() {
     var numGoombas= Number(document.querySelector("#gInput").value);
-    var goombaTotal = numGoombas *goombaCoin;
+    goombaTotal = numGoombas *goombaCoin;
 
     
     document.querySelector("#gOutput").innerHTML = goombaTotal;
-})
+        totalSum();
+});
+
+
+
+
+  //document.querySelector("#gOutput").innerHTML = goombaTotal+bobTotal+cheepTotal;
+//};
+
+//Bob-ombs function
+document.querySelector("#bButton").addEventListener("click", function() {
+    var numBobombs = Number(document.querySelector("#bInput").value);
+    bobOmbTotal = numBobombs * bobOmbCoin;
+    
+    document.querySelector("#bOutput").innerHTML = bobOmbTotal;
+    totalSum();
+});
+
+//Cheep Cheeps function
+document.querySelector("#cButton").addEventListener("click", function() {
+    var cheepCheeps = Number(document.querySelector("#cInput").value);
+    cheepCheepTotal = cheepCheeps * cheepCheepsCoin;
+    
+    document.querySelector("#cOutput").innerHTML = cheepCheepTotal;
+    totalSum();
+});
+
+var totalSum = function () {
+    document.querySelector("#grandTotal").innerHTML = goombaTotal + cheepCheepTotal + bobOmbTotal;
+
 };
 
-
-  
-
-  document.querySelector("#gOutput").innerHTML = goombaTotal+bobTotal+cheepTotal;
